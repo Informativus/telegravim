16074f53d264682781cf3b78efd356dc078a404b
Informativus <ivanpopov123321123@gmail.com>
2026-09-07 12:55:17 +0300

    Prepare the text features release 2026.09.07-101

--- Telegram/SourceFiles/history/view/history_view_message.h
+++ Telegram/SourceFiles/history/view/history_view_message.h
@@ -542,8 +542,8 @@ private:
 	mutable std::unique_ptr<Ui::RoundCheckbox> _selectionRoundCheckbox;
 	mutable uint32 _fromNameVersion : 16 = 0;
 	uint32 _nonTextMaxWidth : 16 = 0;
-	mutable int _bubbleTextualWidthMinimum : 16 = -1;
-	mutable int _bubbleTextualWidthCache : 16 = 0;
+	mutable signed int _bubbleTextualWidthMinimum : 16 = -1;
+	mutable signed int _bubbleTextualWidthCache : 16 = 0;
 	uint32 _bubbleWidthLimit : 26 = 0;
 	uint32 _invertMedia : 1 = 0;
 	uint32 _hideReply : 1 = 0;
